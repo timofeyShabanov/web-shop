3d7db640df4746e355cd481ed26479ac00202261
timofeyShabanov <shabanov.timosha@gmail.com>
2024-09-19 10:42:37 +0300

    main project

--- frontend/src/pages/home/home.tsx
+++ frontend/src/pages/home/home.tsx
@@ -1,9 +1,11 @@
 import './home.scss';
 import ThemeSwitcher from '../../components/ThemeSwitcher/ThemeSwitcher.tsx';
+import Header from '../../components/Header/Header.tsx';
 
 const Home = () => {
   return (
     <>
+      <Header />
       <div>This is home page</div>
       <ThemeSwitcher />
     </>
